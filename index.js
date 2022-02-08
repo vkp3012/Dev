79dd7f09ee1343900e4fc121e161092315bac8bb
vkp3012 <vivekpatel301295@gmail.com>
2022-02-08 23:04:57 +0530

    create index.js and index.html file

---
+++ index.js
@@ -0,0 +1,44 @@
+
+// // variable declaration
+// var a;
+
+// // Variable Initialization
+// a = 10
+// //print in js
+// console.log(a)
+
+// //find type of varibale
+// console.log(typeof a)
+
+// a ="Hello"
+// console.log(a)
+// console.log(typeof a)
+
+// a =true;
+// console.log(a)
+// console.log(typeof a)
+
+// a =null;
+// console.log(a)
+// console.log(typeof a)
+
+//Number
+var num = 10;
+console.log(num);
+
+//double or float
+var float = 2.4;
+console.log(float)
+
+//boolean
+var t = true;
+var f = false;
+console.log(t);
+console.log(f);
+
+//string
+var str = "a"
+console.log(typeof str);
+
+str = "How you doing ?"
+console.log(str);
