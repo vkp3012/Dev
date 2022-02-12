cfa984d4a5415ca65ba5cfd898632ea784534aa9
vkp3012 <vivekpatel301295@gmail.com>
2022-02-12 23:08:46 +0530

    Add calculater function

--- JavaScripts/function.js
+++ JavaScripts/function.js
@@ -18,9 +18,25 @@
 // SayHi("Hello World!");
 // SayHi([1,2,3,4,5])
 
-function add(a,b){
-    return a+b;
+// function add(a,b){
+//     return a+b;
+// }
+
+// let ans = add(2,6);
+// console.log(ans)
+//--------------------------------------------------------
+
+// Function can be returned
+// Functions can be passed as parameters/argument.......
+
+function calculater(str,a,b){
+    if(str=="add"){
+        return function add(){
+            console.log( a+b );
+        }
+    }
 }
 
-let ans = add(2,6);
-console.log(ans)
+let returnedFunc = calculater("add",2,3);
+console.log("returned function is\n" + returnedFunc)
+returnedFunc();
