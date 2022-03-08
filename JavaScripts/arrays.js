bb9b8e14503294c45c75e0755bbdd2122ec84076
vkp3012 <vivekpatel301295@gmail.com>
2022-03-08 21:05:54 +0530

    modify array

--- JavaScripts/arrays.js
+++ JavaScripts/arrays.js
@@ -116,6 +116,17 @@ console.log(array[1]);
 console.log(array[2]);
 //--------------------------------------------------------
 
+// Modify Array Data With Indexes
+// Unlike strings, the entries of arrays are mutable and can be changed freely, even if the array was declared with const.
+
+// Example
+
+// const ourArray = [50, 40, 30];
+// ourArray[0] = 15;
+// ourArray now has the value [15, 40, 30].
+
+// Note: There shouldn't be any spaces between the array name and the square brackets, like array [0]. Although JavaScript is able to process this correctly, this may confuse other programmers reading your code.
+
 /*...................Arrays are Objects...........................*/
 //Arrays are a special type of objects. 
 //The typeof operator in JavaScript returns "object" for arrays.But, JavaScript arrays are best described as arrays.
