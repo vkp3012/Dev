b152cb464f9b9bfa65a10a6605997622954d1256
vkp3012 <vivekpatel301295@gmail.com>
2022-03-08 23:49:52 +0530

    wcat project 2 parts completed

--- Node/wcat/wcat.js
+++ Node/wcat/wcat.js
@@ -1,7 +1,9 @@
 // 1) node wcat.js filepath => displays the contents of a file in terminal
 // 2) node wcat.js filepath1 filepath2 filepath3  => displays the content of all files in terminal in concatinated form in given order
+// 3) node wcat.js -n file1 file 2 file3 OR node wcat.js -n file1 
 //node wcat.js f1.txt
 //node wcat.js f1.txt f2.txt f3.txt
+
 //reqire fs module
 const fs = require("fs");
 
@@ -9,9 +11,17 @@ const fs = require("fs");
 let inputArr = process.argv.slice(2);
 
 let fileArr = [];
+let optionsArr = [];
 //placed files in fileArr
 for(let i = 0;i<inputArr.length;i++){
-    fileArr.push(inputArr[i]);
+    let firstChar = inputArr[i].charAt(0);
+    // console.log(firstChar);
+    if (firstChar == '-') {
+        optionsArr.push(inputArr[i]);
+    }
+    else {
+        fileArr.push(inputArr[i]);
+    }
 }
 
 
@@ -32,3 +42,6 @@ for(let i =0;i<fileArr.length;i++){
 }
 
 console.log(content);
+
+let contentArr = content.split("\n");
+console.log(contentArr);
