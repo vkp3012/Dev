203cb066b9ec8dd8100e3ba51dbe0d1df4dfc712
vkp3012 <vivekpatel301295@gmail.com>
2022-03-22 09:41:42 +0530

    Switch is completed

---
+++ JavaScripts/switch/ifelseToswitch.js
@@ -0,0 +1,62 @@
+/*..................Replacing If Else Chains with Switch........................*/
+/*
+    If you have many options to choose from, a switch statement can be easier to write than 
+    many chained if/else if statements. The following:
+
+if (val === 1) {
+  answer = "a";
+} else if (val === 2) {
+  answer = "b";
+} else {
+  answer = "c";
+}
+
+
+can be replaced with:
+
+switch(val) {
+  case 1:
+    answer = "a";
+    break;
+  case 2:
+    answer = "b";
+    break;
+  default:
+    answer = "c";
+}
+
+Change the chained if/else if statements into a switch statement.
+*/
+
+function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  
+    switch(val) {
+      case "bob":
+        answer = "Marley";
+        break;
+      case 42:
+        answer = "The Answer";
+        break;
+      case 1:
+        answer = "There is no #1";
+        break;
+      case 99:
+        answer = "Missed me by this much!";
+        break;
+      case "John":
+        answer = " ";
+        break;
+        case 156:
+        answer = " ";
+        break;  
+      default:
+        answer = "Ate Nine";
+    }
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  chainToSwitch(7);
