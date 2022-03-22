135f21ee9f0e4a9723fe51849a1b752db292c295
vkp3012 <vivekpatel301295@gmail.com>
2022-03-22 22:41:24 +0530

    espn scrapping data is started

---
+++ Node/webScrapping/espn_scrapper/main.js
@@ -0,0 +1,25 @@
+let url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595"
+
+const request = require("request");
+const cheerio = require("cheerio");
+const allMatchObj = require("./allMatch");
+request(url,cb);
+
+function cb(err, res, body) {
+    if(err){
+        console.error("error", err);
+    }
+    else {
+        handleHtml(body);
+    }
+}
+
+function handleHtml(html){
+    let selecTool = cheerio.load(html);
+    let anchorElem = selecTool('a[data-hover="View All Results"]');
+    let relativeLink = anchorElem.attr("href");
+    //console.log(relativeLink);
+    let fullLink = "https://www.espncricinfo.com" + relativeLink;
+    //console.log(fullLink);
+    allMatchObj.getAllMatch(fullLink);
+}
