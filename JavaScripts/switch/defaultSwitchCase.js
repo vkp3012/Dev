c3f5f43b2a8c6f16ba08806a6fea7d90ba7b4cd5
vkp3012 <vivekpatel301295@gmail.com>
2022-03-22 09:14:13 +0530

    Switch start and default switch case is started

---
+++ JavaScripts/switch/defaultSwitchCase.js
@@ -0,0 +1,51 @@
+/*...............Adding a Default Option in Switch Statements....................*/
+/*
+    In a switch statement you may not be able to specify all possible values as case statements. 
+    Instead, you can add the default statement which will be executed if no matching case statements are found.
+     Think of it like the final else statement in an if/else chain.
+
+A default statement should be the last case.
+
+switch (num) {
+  case value1:
+    statement1;
+    break;
+  case value2:
+    statement2;
+    break;
+...
+  default:
+    defaultStatement;
+    break;
+}
+
+
+Write a switch statement to set answer for the following conditions:
+a - apple
+b - bird
+c - cat
+default - stuff
+
+*/
+
+function switchOfStuff(val) {
+    let answer = "";
+    switch(val){
+      case "a":
+        console.log(answer = "apple");
+        break;
+      case "b":
+        console.log(answer = "bird");
+        break;
+      case "c":
+        console.log(answer = "cat");
+        break;
+      default:
+        console.log(answer = "stuff");
+        break;
+    }
+    return answer;
+}
+  
+switchOfStuff("a");
+switchOfStuff(17);
