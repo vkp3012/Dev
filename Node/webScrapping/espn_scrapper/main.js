7f19b2c47b4d65fcdbb3eccf684a79545cca5bf0
vkp3012 <vivekpatel301295@gmail.com>
2022-03-23 13:28:43 +0530

    Espn IPL Match 2020-21 Data Scrapping

--- Node/webScrapping/espn_scrapper/main.js
+++ Node/webScrapping/espn_scrapper/main.js
@@ -3,6 +3,7 @@ let url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595"
 const request = require("request");
 const cheerio = require("cheerio");
 const allMatchObj = require("./allMatch");
+
 request(url,cb);
 
 function cb(err, res, body) {
@@ -17,7 +18,9 @@ function cb(err, res, body) {
 function handleHtml(html){
     let selecTool = cheerio.load(html);
     let anchorElem = selecTool('a[data-hover="View All Results"]');
-    let relativeLink = anchorElem.attr("href");
+    //console.log(anchorElem);
+    //Attr methods -> Method for getting all attributes and their values
+    relativeLink = anchorElem.attr("href");
     //console.log(relativeLink);
     let fullLink = "https://www.espncricinfo.com" + relativeLink;
     //console.log(fullLink);
--- Node/webScrapping/espn_scrapper/scorecards.js
+++ Node/webScrapping/espn_scrapper/scorecards.js
@@ -2,8 +2,43 @@ const request = require("request");
 const cheerio = require("cheerio");
 
 function getInfoFromScorecard(url) {
-  console.log("from scorecards.js ",url);
-//   request(url, cb);
+    //console.log("from scorecards.js ",url);
+    //we have a url of a scorecard, we want to get html of that scorecard
+    request(url, cb);
+}
+
+function cb(err,res,body){
+    if(err){
+        console.log(err);
+    }else{
+        getMatchDetails(body);
+    }
+}
+
+function getMatchDetails(html){
+    // selecTool conatains html of ith scorecard
+    let selecTool = cheerio.load(html);
+
+    //1. get venue
+    //2.get date
+    let desc = selecTool(".match-header-info.match-info-MATCH");
+    //console.log(desc.text());
+    let descArr = desc.text().split(",");
+    //Match(N), Abu Dhabi, Oct 25 2020, Indian Premier League
+    //console.log(descArr);
+    let dateOfMatch = descArr[2];
+    let venueOfMatch = descArr[1];
+    let noOfMatch = descArr[0];
+    console.log("Number of Match ",noOfMatch);
+    console.log("Date of Match ",dateOfMatch);
+    console.log("Venue of Match ",venueOfMatch);
+
+    //3. get result
+    let matchResEle = selecTool(".match-info.match-info-MATCH.match-info-MATCH-half-width>.status-text");
+    console.log("Result ",matchResEle.text());
+    console.log("-------------------------------------");
+
+    //4. get team names
 }
 
 module.exports = {
