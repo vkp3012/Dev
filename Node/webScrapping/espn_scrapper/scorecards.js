66a09a39b482c5259cf35748b9424814cb5ed008
vkp3012 <vivekpatel301295@gmail.com>
2022-03-30 09:17:37 +0530

    :tada: v2.0 is update code

--- Node/webScrapping/espn_scrapper/scorecards.js
+++ Node/webScrapping/espn_scrapper/scorecards.js
@@ -15,48 +15,56 @@ function getInfoFromScorecard(url) {
 function cb(err,res,body){
     if(err){
         console.log(err);
+    }else if(res.statusCode == 404){
+        console.log("Page Not Found");
     }else{
+        // console.log("page found");
         getMatchDetails(body);
     }
 }
 
+
 function getMatchDetails(html){
+
     // selecTool conatains html of ith scorecard
     let selecTool = cheerio.load(html);
-
-    //1. get venue
-    //2.get date
+    
+    //select class of venue and date
     let desc = selecTool(".match-header-info.match-info-MATCH");
     //console.log(desc.text());
-    let descArr = desc.text().split(",");
-    //Match(N), Abu Dhabi, Oct 25 2020, Indian Premier League
-    //console.log(descArr);
-    let dateOfMatch = descArr[2];
-    let venueOfMatch = descArr[1];
-    let noOfMatch = descArr[0];
-    console.log("Number of Match ",noOfMatch);
-    console.log("Date of Match ",dateOfMatch);
-    console.log("Venue of Match ",venueOfMatch);
+    //output -> Match(N), Abu Dhabi, Oct 25 2020, Indian Premier League
 
-    //3. get result
+    //split text format 
+    let descArr = desc.text().split(",");
+    //console.log(descArr);
+
+    //1.get date
+    let dateOfMatch = descArr[2];
+    console.log("Date of Match ",dateOfMatch);
+
+    //2. get venue
+    let venueOfMatch = descArr[1];
+    console.log("Venue of Match ",venueOfMatch);
+    
+
+    //3. select class of result 
     let matchResEle = selecTool(".match-info.match-info-MATCH.match-info-MATCH-half-width>.status-text");
     let matchResult = matchResEle.text();
     console.log(matchResult);
     
 
-    //4. get team names
+    //4. select class of team names
     let teamNames = selecTool(".name-detail>.name-link");
     // console.log(teamNames.text());
 
     let ownTeam = selecTool(teamNames[0]).text();
     let opponentTeam = selecTool(teamNames[1]).text();
-    console.log("Team 1", ownTeam);
-    console.log("Team 2",opponentTeam);
-    console.log("-------------------------------------");
+    // console.log("Team 1", ownTeam);
+    // console.log("Team 2",opponentTeam);
+    // console.log("-------------------------------------");
 
-//5. get innings 
-let allBatsmenTable = selecTool(".table.batsman tbody");
-  //console.log("number of batsmen tables are ->   ",allBatsmenTable.length);
+  //5. select class of innings 
+  let allBatsmenTable = selecTool(".table.batsman tbody");
   //let htmlString = "";
   //let count = 0;
   for (let i = 0; i < allBatsmenTable.length; i++) {
@@ -64,11 +72,20 @@ let allBatsmenTable = selecTool(".table.batsman tbody");
     //Get the descendants(table rows) of each element (table )
     let allRows = selecTool(allBatsmenTable[i]).find("tr"); // -> data of batsmen + empty rows 
     
+    //let temp;
+    if(i == 1){
+      let temp = ownTeam;
+      ownTeam = opponentTeam;
+      opponentTeam = temp;
+    }
+    console.log(ownTeam);
+    console.log(opponentTeam);
+
     for (let j = 0; j < allRows.length; j++) {
       //Check to see if any of the matched elements have the given className
       let row = selecTool(allRows[j]);
-      let allcol = row.find("td");
-      if (selecTool(allcol[0]).hasClass("batsman-cell")) {
+      let allcol = row.find("td")[0];
+      if (selecTool(allcol).hasClass("batsman-cell")) {
         //will be getting valid data
         // count++;
         // console.log("inside " + count);
@@ -80,12 +97,28 @@ let allBatsmenTable = selecTool(".table.batsman tbody");
         //   }
         // }
         // console.log(playerName);
-        let playerName = selecTool(allcol[0]).text();
-        let runs = selecTool(allcol[2]).text();
-        let balls = selecTool(allcol[3]).text();
-        let numberOf4 = selecTool(allcol[5]).text();
-        let numberOf6 = selecTool(allcol[6]).text();
-        let sr = selecTool(allcol[7]).text();
+
+        let pn = selecTool(row.find("td")[0]).text().split("");
+        // console.log(pn);
+        // console.log(pn.join(""));
+        let playerName = "";
+        //Determines whether an array includes a certain element, returning true or false as appropriate.
+        if(pn.includes("(")){
+          playerName = pn.join("").split("(")[0];
+          // console.log(playerName);
+        }else if(pn.includes("†")){
+          playerName = pn.join("").split("†")[0];
+          // console.log(playerName);
+        }else{
+          playerName = pn.join("");
+        }
+
+        // let playerName = selecTool(allcol[0]).text();
+        let runs = selecTool(row.find("td")[2]).text();
+        let balls = selecTool(row.find("td")[3]).text();
+        let numberOf4 = selecTool(row.find("td")[5]).text();
+        let numberOf6 = selecTool(row.find("td")[6]).text();
+        let sr = selecTool(row.find("td")[7]).text();
 
         console.log(
           `playerName -> ${playerName} runsScored ->  ${runs} ballsPlayed ->  ${balls} numbOfFours -> ${numberOf4} numbOfSixes -> ${numberOf6}  strikeRate-> ${sr}`
@@ -104,15 +137,18 @@ let allBatsmenTable = selecTool(".table.batsman tbody");
           numberOf6,
           sr
         );
+
+        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
       }
     }
   }   
   // console.log(htmlString);
 
-
   function processInformation(dateOfMatch,venueOfMatch,matchResult,ownTeam,opponentTeam,playerName,runs,balls,numberOf4,numberOf6,sr){
     let teamNamePath = path.join(__dirname,"IPL",ownTeam);
-    dirCreater(teamNamePath)
+    if(!fs.existsSync(teamNamePath)){
+      fs.mkdirSync(teamNamePath);
+    }
     
     let playerPath = path.join(teamNamePath,playerName + ".xlsx");
     let content = excelReader(playerPath,playerName);
@@ -136,12 +172,7 @@ let allBatsmenTable = selecTool(".table.batsman tbody");
     excelWriter(playerPath,content,playerName);
   }
   // console.log(htmlstring);
-}
 
-function dirCreater(teamNamePath){
-  if(!fs.existsSync(teamNamePath)){
-    fs.mkdirSync(teamNamePath);
-  }
 }
 
 //this function reads the data from excel file
