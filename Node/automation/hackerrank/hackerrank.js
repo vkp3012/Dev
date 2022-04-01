19b22835f7395388212a207aeae7f588eb6c042c
vkp3012 <vivekpatel301295@gmail.com>
2022-04-01 09:11:02 +0530

    :art: algo is opened

--- Node/automation/hackerrank/hackerrank.js
+++ Node/automation/hackerrank/hackerrank.js
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer")
-let email = "vivekpatel301295@gmail.com"
-let password = ""
+let {email,password} = require("./secrets")
+// let email = ""
+// let password = ""
 let cTab;
 let browserOpenPromise = puppeteer.launch(
     {
@@ -41,7 +42,36 @@ browserOpenPromise
     })
     .then(function(){
         console.log("Logging Hackerrank is succesfuly");
+        //waitAndClick will wait for the selector to load , and then click on the node
+        let algorithmTabWillBeOpenedPromise = waitAndClick("div[data-automation='algorithms']");
+        return algorithmTabWillBeOpenedPromise;
+    })
+    .then(function(){
+        console.log("algorithm pagees is open");
     })
     .catch(function(err){
         console.log(err);
-    })
+    });
+    function waitAndClick(algoBtn){
+        let waitClickPromise = new Promise(function(resolve,reject){
+            let waitForSelectorPromise = cTab.waitForSelector(algoBtn);
+            waitForSelectorPromise
+                .then(function(){
+                    console.log("algo btn is found");
+                    let clickPromise = cTab.click(algoBtn);
+                    return clickPromise;
+                }) 
+                .then(function(){
+                    console.log("algo btn is clicked");
+                    //resolve();
+                }) 
+                .catch(function(err){
+                    console.log(err);
+                })
+        });
+
+        // waitClickPromise.then(function () {
+        //   console.log("inside then of waitclick");
+        // });
+        return waitClickPromise;
+    }
