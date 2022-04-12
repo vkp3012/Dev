87e1644fdeccf823ed0ee761a9ae31ebeb95eb09
vkp3012 <vivekpatel301295@gmail.com>
2022-04-12 22:15:57 +0530

    set time out is study

---
+++ Node/automation/settimeout.js
@@ -0,0 +1,10 @@
+function yolo(){
+    var a = 10;
+    function cb(){
+        console.log("hello how are you?");
+    }
+    setTimeout(cb,5000);
+    console.log(a);
+}
+
+yolo()
--- Node/automation/testProject/test.js
+++ Node/automation/testProject/test.js
@@ -1,10 +1,10 @@
 let link = "https://testproject.io/contact-us/"
 
-let firstname = "Testing UI Automation"
-let lastname = "Test last Name"
-let companymail ="test@testmail.com";
-let phone = "9544486837"
-let message = "This is a test message created by automated testing of UI using Puppeteer";
+// let firstname = "Testing UI Automation"
+// let lastname = "Test last Name"
+// let companymail ="test@testmail.com";
+// let phone = "9544486837"
+// let message = "This is a test message created by automated testing of UI using Puppeteer";
 
 const puppeteer = require("puppeteer")
 let cTab;
@@ -25,10 +25,21 @@ let cTab;
         //Waiting for form to load
         await cTab.waitFor('.wpcf7-form');
 
-        await cTab.type("#firstName",firstname);
-        await cTab.type("#lastName",lastname)
-        await cTab.type("#companyEmail",companymail)
-        await cTab.type(".wpcf7-form-control.wpcf7-text.wpcf7-tel.wpcf7-validates-as-tel",phone);
+        //name
+        let fname = await cTab.waitForSelector("#firstName");
+        await fname.type("vivek")
+
+        //last name
+        let lname=await cTab.waitForSelector("#lastName")
+        await lname.type('patel');
+
+        //email
+        let email=await cTab.waitForSelector("#companyEmail")
+        await email.type("vivel@gmail.com")
+
+        //phone
+        let phone=await cTab.type(".wpcf7-form-control.wpcf7-text.wpcf7-tel.wpcf7-validates-as-tel");
+        await phone.type("99444868387");
 
         //Waiting for dropdown element and clicking on it
         const dropdown = "#select2-subject-container";
@@ -38,8 +49,9 @@ let cTab;
         //Enter on subject
         await cTab.keyboard.press('Enter'); 
 
-
-        await cTab.type(".wpcf7-form-control.wpcf7-textarea.wpcf7-validates-as-required",message)
+        //message
+        let msg = await cTab.type(".wpcf7-form-control.wpcf7-textarea.wpcf7-validates-as-required")
+        await msg.type("This is a test message created by automated testing of UI using Puppeteer")
 
         //Clicking on Contact us
         await cTab.keyboard.press('Tab');
@@ -54,7 +66,7 @@ let cTab;
         //Take screenshot of after submit form UI
         await cTab.screenshot({path: 'aftersubmit.png'});
         
-        //await browserOpen.close();
+        // await browserOpen.close();
 
     } catch (error) {
         console.log(error);
