bc6c4fc6c159452eb755ec9365d36066c8d76a44
vkp3012 <vivekpatel301295@gmail.com>
2022-05-08 09:37:29 +0530

    jira app html and css is work on progress

---
+++ JavaScripts/test/test2.js
@@ -0,0 +1,100 @@
+// Q1
+// const num = 10;
+// console.log(num+5);
+// let a = 6;
+// a = a + num;
+// console.log(num-a);
+
+//Q2
+// let a = 1;
+//     { let a = 2;
+//         { let a = 3;
+//             { let a = 4;
+//               console.log(a);
+//         }
+//         console.log(a);
+//     } 
+//     console.log(a);
+// }
+// console.log(a);
+
+//Q4
+// var num1 = 10;
+// let num2 = 10;
+// {
+// var num1 = 20;
+// let num2 = 20;
+// }
+// console.log("num1:", num1, "num2:", num2);
+
+//Q5
+// console.log("line 4: ", a);
+// var a;
+// console.log("line 6: ", a);
+// a = 10;
+// console.log("line 8: ", a);
+
+//Q6
+// const a = 10;
+// const b = 20 ;
+// b = b + 10;
+// console.log( a + b)
+
+//Q7
+// var a = "Pepcoding";
+// var result = a.substring(3, 7);
+// console.log(result);
+
+//Q8
+// var a = "hello";
+// var sum = 0;
+// for( var i =0; i < a.length ; i++) {
+//     sum += (a[i] -"a");
+// }
+// console.log(sum);
+
+//Q9
+// let obj1 = {
+//     firstName : "Joe",
+//     secondName : "Daem",
+//     age : 5 ,
+//     city : "New York"
+// }
+
+// delete obj1.age;
+// console.log(obj1);
+
+//Q11
+// var a = 29;
+// var b = a = 30;
+// console.log(a);
+
+//Q12
+// (function(a){
+//     return (function(){
+//     console.log(a);
+//     a = 6;
+//     })()
+//     })(12)
+
+
+//Q13
+// var num1 = 10;
+// let num2 = 10;
+// {
+// var num1 = 20;
+// let num2 = 20;
+// console.log("num1:", num1, "num2:", num2);
+// }
+
+//Q14
+// 12 =="12"
+
+//Q15
+
+// console.log("line 4: ", a);
+let a;
+console.log("line 6: ", a);
+a = 10;
+console.log("line 8: ", a);
+
--- Node/automation/hackerrank/hasync.js
+++ Node/automation/hackerrank/hasync.js
@@ -1,37 +1,39 @@
 let link = "https://www.hackerrank.com/auth/login"
 const puppeteer = require("puppeteer");
-let {email,password} = require("./secrets")
+let {email,password} = require("./secrets");
 
-let cTab;
 (async function(){
     try {
         //browser is open
-        let browser = puppeteer.launch({
+        let browserInstant = await puppeteer.launch({
             headless:false,
             defaultViewport:null,
             args:["--start-maximized"]
         })
-        //wait foe browser
-        let browserInstant = await browser;
-        let allTabsArr = await browserInstant.pages();
-        cTab = allTabsArr[0]
+
         //link is open in browser
+        let cTab = await browserInstant.newPage();
         await cTab.goto(link);
         //email type
-        await cTab.type("input[name='username']",email)
+        await cTab.type("input[name='username']",email,{delay:50})
         //password type
-        await cTab.type("input[name='password']",password)
+        await cTab.type("input[name='password']",password,{delay:50})
         //login buttom is click and loging to hackerrank page
-        await cTab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled")
-        //wait for select allgorithim selector page tage
-        await waitAndClick("div[data-automation='algorithms']");
-        await cTab.waitForSelector('a[data-analytics="ChallengeListChallengeName"]')
-        //all link print in algorthim algo
-        let allQuesLink = await cTab.evaluate(getAllQuesLinks,'a[data-analytics="ChallengeListChallengeName"]');
-        //console.log(allQuesLink);
-        await cTab.evaluate(allQuesLink)
+        await cTab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled",{delay:50})
 
-        await questionAllLink(linkArr[0],0);
+        // //wait for select allgorithim selector page tage
+        await waitAndClick("div[data-automation='algorithms']",cTab);
+
+        await waitAndClick('input[value="warmup"]',cTab);
+        let allques = await cTab.$$(".individual-challenge-card-v2.content--list-v2 track_content")
+        console.log(allques.length);
+        // await cTab.waitForSelector('a[data-analytics="ChallengeListChallengeName"]')
+        // //all link print in algorthim algo
+        // let allQuesLink = await cTab.evaluate(getAllQuesLinks,'a[data-analytics="ChallengeListChallengeName"]');
+        // console.log(allQuesLink.length);
+        // await cTab.evaluate(allQuesLink)
+
+        // await questionAllLink(linkArr[0],0);
 
     } catch (error) {
         console.log(error);
@@ -39,28 +41,34 @@ let cTab;
 
 })()
 
-async function waitAndClick(algoOpen){
-    let algo = await cTab.waitForSelector(algoOpen)
-    algo
-        await cTab.click(algoOpen)
+async function waitAndClick(selector,cPage){
+    await cPage.waitForSelector(selector);
+    let selectorClicked = cPage.click(selector);
+    return selectorClicked;
 }
 
-//async function waitForSelecter()
+// async function waitAndClick(algoOpen){
+//     let algo = await cTab.waitForSelector(algoOpen)
+//     algo
+//         await cTab.click(algoOpen)
+// }
 
-async function getAllQuesLinks(select){
-    let allElemArr = document.querySelectorAll(select);
-    let linkArr  =[];
-    for(let i = 0;i<allElemArr.length;i++){
-        linkArr.push(allElemArr[i].getAttribute("href"));
-    }
-    return linkArr;
-}
+// //async function waitForSelecter()
 
-async function questionAllLink(linkArr){
-    await questionSolver(linkArr)
-}
+// async function getAllQuesLinks(select){
+//     let allElemArr = document.querySelectorAll(select);
+//     let linkArr  =[];
+//     for(let i = 0;i<allElemArr.length;i++){
+//         linkArr.push(allElemArr[i].getAttribute("href"));
+//     }
+//     return linkArr;
+// }
 
-async function questionSolver(url,idx){
-    let fullLink = `https://www.hackerrank.com${url}`;
-    await cTab.goto(fullLink);
-}
+// async function questionAllLink(linkArr){
+//     await questionSolver(linkArr)
+// }
+
+// async function questionSolver(url,idx){
+//     let fullLink = `https://www.hackerrank.com${url}`;
+//     await cTab.goto(fullLink);
+// }
