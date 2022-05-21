216291a06699f37f1025f68471be1f4f8d1dc22e
vkp3012 <vivekpatel301295@gmail.com>
2022-05-21 23:36:59 +0530

    camera and gallery project is started

---
+++ browser/camera/script.js
@@ -0,0 +1,11 @@
+let video = document.querySelector("video");
+
+let constraints={
+    video:true,
+    audio:true
+}
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then((stream)=>{
+    video.srcObject = stream,
+})
